ac15f0516e87ba210b5c05260a726fd7a37b8770
tato88 <380685013386@ex.ua>
2022-06-29 16:36:48 +0300

    add car

---
+++ apps/cars/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class CarModel(models.Model):
+    class Meta:
+        db_table = 'cars'
+
+    brand = models.CharField(max_length=100)
+    price = models.IntegerField()
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.brand
---
+++ apps/cars/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from .models import CarModel
+from .serializers import CarSerializer
+
+
+class CarsListCreateView(ListCreateAPIView):
+    queryset = CarModel.objects.all()
+    serializer_class = CarSerializer
+
+
+class CarsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = CarModel.objects.all()
+    serializer_class = CarSerializer
