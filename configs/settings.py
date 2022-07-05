a39d0a6101de3c601c08df37b6769fb6aa83c751
tato88 <380685013386@ex.ua>
2022-07-05 22:32:08 +0300

    loginazi

--- configs/settings.py
+++ configs/settings.py
@@ -29,6 +29,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000'
 ]
 
+AUTH_USER_MODEL = 'users.UserModel'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
 
     # Apps
+    'apps.users',
     'apps.cars',
     'apps.auto_parks'
 ]
@@ -49,7 +51,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
